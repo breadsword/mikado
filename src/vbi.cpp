8186dd5deadfdd586d870af23b7a019c1d7a5164
Andreas Pfeil <andreas_pfeil@yahoo.de>
2020-11-21 11:31:50 +0100

    move implementation of vbi to vbi.cpp

---
+++ src/vbi.cpp
@@ -0,0 +1,12 @@
+#include "vbi.h"
+
+
+bool mikado::vbi_decoder::read_byte(mikado::byte b)
+{
+    value += ((b & 0x7F) << (7*multiplier));
+    multiplier ++;
+
+    more_to_read = (b >> 7);
+
+    return multiplier < 4;
+}
