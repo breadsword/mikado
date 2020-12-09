fad810656a93538955b3cc64c92c7b5e9d0500ac
Andreas Pfeil <andreas_pfeil@yahoo.de>
2020-12-09 22:52:27 +0100

    add filename for logging

--- examples/log.h
+++ examples/log.h
@@ -2,9 +2,9 @@
 #define LOG_H
 
 #include <iostream>
-// #define LOG std::cout << __FILE__ << "(" << __LINE__ << ")" << ": "
+ #define LOG std::cout << __FILE__ << "(" << __LINE__ << ")" << ": "
 // #define LOG std::cout << __FILE_NAME__ << "(" << __LINE__ << ")" << ": "
-#define LOG std::cout << __FUNCTION__ << "(" << __LINE__ << ")" << ": "
+//#define LOG std::cout << __FUNCTION__ << "(" << __LINE__ << ")" << ": "
 using std::endl;
 
 
